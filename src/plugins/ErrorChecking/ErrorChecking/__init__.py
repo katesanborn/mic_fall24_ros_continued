ef3672128ddcea8f53328689fa4f953747f151de
Kate Sanborn <kate.l.sanborn@vanderbilt.edu>
2024-12-11 15:10:31 -0600

    Added plugin for error checking launch files

---
+++ src/plugins/ErrorChecking/ErrorChecking/__init__.py
@@ -0,0 +1,67 @@
+"""
+This is where the implementation of the plugin code goes.
+The ErrorChecking-class is imported from both run_plugin.py and run_debug.py
+"""
+import sys
+import logging
+from webgme_bindings import PluginBase
+
+# Setup a logger
+logger = logging.getLogger('ErrorChecking')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)  # By default it logs to stderr..
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+class ErrorChecking(PluginBase):
+    def main(self):
+        core = self.core
+        root_node = self.root_node
+        active_node = self.active_node
+
+        # name = core.get_attribute(active_node, 'name')
+
+        # logger.info('ActiveNode at "{0}" has name {1}'.format(core.get_path(active_node), name))
+
+        # core.set_attribute(active_node, 'name', 'newName')
+
+        # commit_info = self.util.save(root_node, self.commit_hash, 'master', 'Python plugin updated the model')
+        # logger.info('committed :{0}'.format(commit_info))
+        
+        def get_type(node):
+            meta_types = ["LaunchFile", "Include", "Argument", "Remap", "Group", "Parameter", "rosparam", "Node", "Topic", "GroupPublisher", "GroupSubscriber", "Subscriber", "Publisher", "Machine", "Env", "Test", "rosparamBody"]
+            base_type = core.get_base(node)
+            while base_type and core.get_attribute(base_type, 'name') not in meta_types:
+                base_type = core.get_base(base_type)
+            return core.get_attribute(base_type, 'name')
+        
+        all_nodes = {}
+        
+        def at_node(node):
+            node_type = get_type(node)
+            
+            if all_nodes.get(node_type) == None:
+                all_nodes[node_type] = [node]
+            else:
+                all_nodes[node_type].append(node)
+                
+        self.util.traverse(active_node, at_node)
+        
+        # Check duplicate names in nodes and tests
+        logger.info("TESTING FOR DUPLICATE NAMES IN NODES AND TESTS:\n============================")
+        test_node_names = []
+        for node in all_nodes.get("Node"):
+            test_node_names.append(core.get_attribute(node, "name"))
+            
+        for test in all_nodes.get("Test"):
+            test_node_names.append(core.get_attribute(test, "testName"))
+            
+        duplicate_names = {name for name in test_node_names if test_node_names.count(name) > 1}
+        
+        if len(duplicate_names) > 0:
+            logger.info(f"Found duplicate names in nodes and tests: {duplicate_names}")
+        else:
+            logger.info("No duplicate names")
