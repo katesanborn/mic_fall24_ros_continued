a0f1ebee48f6ef6950a97987f04824cb63f8b638
Kate Sanborn <kate.l.sanborn@vanderbilt.edu>
2024-12-12 15:38:58 -0600

    Added config file input and code to build libraries to update library plug in

--- src/plugins/UpdateLibrary/UpdateLibrary/__init__.py
+++ src/plugins/UpdateLibrary/UpdateLibrary/__init__.py
@@ -5,6 +5,7 @@ The UpdateLibrary-class is imported from both run_plugin.py and run_debug.py
 import sys
 import logging
 from webgme_bindings import PluginBase
+import json
 
 # Setup a logger
 logger = logging.getLogger('UpdateLibrary')
@@ -22,11 +23,102 @@ class UpdateLibrary(PluginBase):
         root_node = self.root_node
         active_node = self.active_node
 
-        name = core.get_attribute(active_node, 'name')
+        # name = core.get_attribute(active_node, 'name')
 
-        logger.info('ActiveNode at "{0}" has name {1}'.format(core.get_path(active_node), name))
+        # logger.info('ActiveNode at "{0}" has name {1}'.format(core.get_path(active_node), name))
 
-        core.set_attribute(active_node, 'name', 'newName')
+        # core.set_attribute(active_node, 'name', 'newName')
 
-        commit_info = self.util.save(root_node, self.commit_hash, 'master', 'Python plugin updated the model')
-        logger.info('committed :{0}'.format(commit_info))
+        # commit_info = self.util.save(root_node, self.commit_hash, 'master', 'Python plugin updated the model')
+        # logger.info('committed :{0}'.format(commit_info))
+        
+        # Load library config
+        config = self.get_current_config()
+        library_config = self.get_file(config['file'])
+        
+        # Find libraries for node, test, and include and load current elements
+        all_children = core.load_sub_tree(active_node)
+        
+        node_lib = next((child for child in all_children if core.get_attribute(child, "name") == "NodeLibrary"), None)
+        if not node_lib:
+            logger.error("NodeLibrary not found.")
+            return
+        
+        test_lib = next((child for child in all_children if core.get_attribute(child, "name") == "TestLibrary"), None)
+        if not test_lib:
+            logger.error("TestLibrary not found.")
+            return
+        
+        include_lib = next((child for child in all_children if core.get_attribute(child, "name") == "IncludeLibrary"), None)
+        if not include_lib:
+            logger.error("IncludeLibrary not found.")
+            return
+
+        logger.info(f"Node library: {node_lib}")
+        logger.info(f"Test library: {test_lib}")
+        logger.info(f"Include library: {include_lib}")
+        
+        node_lib_children = core.load_children(node_lib)
+        
+        test_lib_children = core.load_children(test_lib)
+        
+        include_lib_children = core.load_children(include_lib)
+        
+        # Delete current library contents
+        for node in node_lib_children:
+            core.delete_node(node)
+            
+        for test in test_lib_children:
+            core.delete_node(test)
+            
+        for include in include_lib_children:
+            core.delete_node(include)
+            
+        # Set up config as json
+        library_config_json = json.loads(library_config)
+        
+        # Parse json to build libraries
+        for package in library_config_json:
+            for node in package["nodes"]:
+                # Creating a new node
+                new_node = core.create_child(node_lib, self.META.get("Node", None))
+                core.set_attribute(new_node, "name", node["node"])
+                core.set_attribute(new_node, "type", node["node"])
+                core.set_attribute(new_node, "pkg", package["package"])
+                
+                # Creating a new test
+                new_test = core.create_child(test_lib, self.META.get("Test", None))
+                core.set_attribute(new_test, "testName", node["node"])
+                core.set_attribute(new_test, "name", "test_" + node["node"])
+                core.set_attribute(new_test, "type", node["node"])
+                core.set_attribute(new_test, "pkg", package["package"])
+                
+                # Adding publishers
+                if node["publishers"] is not None:
+                    for pub in node["publishers"]:
+                        node_pub = core.create_child(new_node, self.META.get("Publisher", None))
+                        core.set_attribute(node_pub, "name", pub)
+                        
+                        test_pub = core.create_child(new_test, self.META.get("Publisher", None))
+                        core.set_attribute(test_pub, "name", pub)
+                    
+                # Adding subscribers
+                if node["subscribers"] is not None:
+                    for sub in node["subscribers"]:
+                        node_sub = core.create_child(new_node, self.META.get("Subscriber", None))
+                        core.set_attribute(node_sub, "name", sub)
+                        
+                        test_sub = core.create_child(new_test, self.META.get("Subscriber", None))
+                        core.set_attribute(test_sub, "name", sub)
+            
+            for launch_file in package["launch_files"]:
+                new_include = core.create_child(include_lib, self.META.get("Include", None))
+                core.set_attribute(new_include, "name", f"$(find {package["package"]})/" + launch_file["relative_path"])
+        
+        # Save updates
+        new_commit_hash = self.util.save(core.load_root(self.project.get_root_hash(self.commit_hash)), self.commit_hash)    
+        self.project.set_branch_hash(
+            branch_name=self.branch_name,
+            new_hash=new_commit_hash["hash"],
+            old_hash=self.commit_hash
+        )
