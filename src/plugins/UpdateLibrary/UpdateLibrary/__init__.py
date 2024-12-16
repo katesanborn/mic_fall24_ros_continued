c691e60a2012118229632c2a19423ac4c361a73f
Kate Sanborn <kate.l.sanborn@vanderbilt.edu>
2024-12-16 08:53:55 -0600

    Removed comments

--- src/plugins/UpdateLibrary/UpdateLibrary/__init__.py
+++ src/plugins/UpdateLibrary/UpdateLibrary/__init__.py
@@ -22,15 +22,6 @@ class UpdateLibrary(PluginBase):
         core = self.core
         root_node = self.root_node
         active_node = self.active_node
-
-        # name = core.get_attribute(active_node, 'name')
-
-        # logger.info('ActiveNode at "{0}" has name {1}'.format(core.get_path(active_node), name))
-
-        # core.set_attribute(active_node, 'name', 'newName')
-
-        # commit_info = self.util.save(root_node, self.commit_hash, 'master', 'Python plugin updated the model')
-        # logger.info('committed :{0}'.format(commit_info))
         
         # Load library config
         config = self.get_current_config()
