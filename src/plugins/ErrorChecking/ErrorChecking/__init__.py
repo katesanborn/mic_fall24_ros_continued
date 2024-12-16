1ce561f5b57b8614e6b63e56201124e41075c9e1
Kate Sanborn <kate.l.sanborn@vanderbilt.edu>
2024-12-16 10:32:48 -0600

    Added comments to error checking plug in

--- src/plugins/ErrorChecking/ErrorChecking/__init__.py
+++ src/plugins/ErrorChecking/ErrorChecking/__init__.py
@@ -23,37 +23,57 @@ class ErrorChecking(PluginBase):
         core = self.core
         active_node = self.active_node
 
+        # Stores report of all errors for notification
         error_report = ""
-        divider = ":"
+        # Divides test name from result in error report
+        divider = ": "
         
-        def get_type(node):
+        def get_type(node: dict) -> str:
+            """Returns the type of the WebGME node
+
+            Args:
+                node (dict): A node in the WebGME project
+
+            Returns:
+                str: The type of the node as defined in the Launch File tab in the metamodel
+            """            
             meta_types = ["LaunchFile", "Include", "Argument", "Remap", "Group", "Parameter", "rosparam", "Node", "Topic", "GroupPublisher", "GroupSubscriber", "Subscriber", "Publisher", "Machine", "Env", "Test", "rosparamBody"]
             base_type = core.get_base(node)
             while base_type and core.get_attribute(base_type, 'name') not in meta_types:
                 base_type = core.get_base(base_type)
             return core.get_attribute(base_type, 'name')
         
+        # Dictionary to store all nodes using type as key
         all_nodes = {}
         
-        def at_node(node):
+        def document_nodes(node: dict):
+            """Stores all nodes in project in dictionary with type as key
+
+            Args:
+                node (dict): A node in the WebGME project
+            """            
             node_type = get_type(node)
             
-            if all_nodes.get(node_type) == None:
+            if all_nodes.get(node_type) is None:
                 all_nodes[node_type] = [node]
             else:
                 all_nodes[node_type].append(node)
                 
-        self.util.traverse(active_node, at_node)
+        self.util.traverse(active_node, document_nodes)
         
         # Check duplicate names in nodes and tests
         error_report += "TESTING FOR DUPLICATE NAMES IN NODES AND TESTS" + divider
         test_node_names = []
-        for node in all_nodes.get("Node"):
-            test_node_names.append(core.get_attribute(node, "name"))
+        
+        if all_nodes.get("Node") is not None:
+            for node in all_nodes.get("Node"):
+                test_node_names.append(core.get_attribute(node, "name"))
             
-        for test in all_nodes.get("Test"):
-            test_node_names.append(core.get_attribute(test, "testName"))
-            
+        if all_nodes.get("Test") is not None:
+            for test in all_nodes.get("Test"):
+                test_node_names.append(core.get_attribute(test, "testName"))
+
+        # Make list of all node names that are repeated 
         duplicate_names = {name for name in test_node_names if test_node_names.count(name) > 1}
         
         if len(duplicate_names) > 0:
@@ -64,15 +84,18 @@ class ErrorChecking(PluginBase):
         # Check that an argument does not have both default and value defined
         error_report += "TESTING FOR ERRORS IN ARG DEFINITION" + divider
         
+        # List to store all argument nodes in project with errors
         args_with_error = []
         
-        for arg in all_nodes.get("Argument"):
-            name = core.get_attribute(arg, "name")
-            default = core.get_attribute(arg, "default")
-            value = core.get_attribute(arg, "value")
-            
-            if default and value:
-                args_with_error.append(name)
+        # Find nodes with default and value defined and add to errors
+        if all_nodes.get("Argument") is not None:
+            for arg in all_nodes.get("Argument"):
+                name = core.get_attribute(arg, "name")
+                default = core.get_attribute(arg, "default")
+                value = core.get_attribute(arg, "value")
+                
+                if default and value:
+                    args_with_error.append(name)
                 
         if len(args_with_error) > 0:
             error_report += f"Found args with default and value defined: {args_with_error} |"
@@ -82,18 +105,28 @@ class ErrorChecking(PluginBase):
         # Check that arguments do not have a circular dependency
         error_report += "TESTING FOR CIRCULAR DEPENDENCIES IN ARG DEFINITION" + divider
         
-        def get_arg_from_string(arg_string):
+        def get_arg_from_string(arg_string: str) -> list[str]:
+            """Extract all names in string in form $(arg name)
+
+            Args:
+                arg_string (str): String to check for arguments
+
+            Returns:
+                list[str]: list of arguments found in string (if any)
+            """            
             pattern = r"\$\(\s*arg\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*\)"
             return re.findall(pattern, arg_string)
         
+        # Dictionary to store args and the args that they depend on
         precedence = {}
         
-        for arg in all_nodes.get("Argument"):
-            name = core.get_attribute(arg, "name")
-            default = core.get_attribute(arg, "default")
-            value = core.get_attribute(arg, "value")
-            
-            precedence[name] = get_arg_from_string(default) + get_arg_from_string(value)
+        if all_nodes.get("Argument") is not None:
+            for arg in all_nodes.get("Argument"):
+                name = core.get_attribute(arg, "name")
+                default = core.get_attribute(arg, "default")
+                value = core.get_attribute(arg, "value")
+                
+                precedence[name] = get_arg_from_string(default) + get_arg_from_string(value)
         
         try:
             ts = TopologicalSorter(precedence)
